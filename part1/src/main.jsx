eb4eee9c50e23e687afe23daeaabde512186e915
hmedo <hmedo@7medo>
2024-11-01 22:01:13 +0200

    testing console on browser

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -7,9 +7,3 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
