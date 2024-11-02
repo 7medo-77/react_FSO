59132a2581e87cce9135c9aaba1d9db46d630188
hmedo <hmedo@7medo>
2024-11-02 05:47:52 +0200

    part 1c destructuring

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,15 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Hello = (props) => {
-  const bornYear = () => {
-    const thisYear = new Date().getFullYear();
-    return thisYear - props.age;
-  }
+const Hello = ({ name, age }) => {
+  const bornYear = () =>  new Date().getFullYear() - age
   return (
     <>
       <p>
-        Hello, {props.name}, you are {props.age} years old!
+        Hello, {name}, you are {age} years old!
       </p>
       <p>
         You were born in {bornYear()}
@@ -23,14 +20,10 @@ const Hello = (props) => {
 const App = () => {
   const name = 'Peter';
   const age = 10;
-  const nameArray = ['Ahmed', 'Mohamed', 'Mahmoud'];
 
   return (
     <div>
       <h1> Greetings! </h1>
-      <p>
-        {nameArray}
-      </p>
       <Hello name='Maya' age={26 + 10} />
       <Hello name={name} age={age} />
     </div>
