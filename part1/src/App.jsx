2702bb30dd2fe23260e2db45fba4cb999c283013
hmedo <hmedo@7medo>
2024-11-03 00:15:43 +0200

    part 1c passing props to children

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,17 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const CounterButton = (props) => {
+  console.log(props);
+
+  return (
+    <button onClick={props.onClick} >{props.text}</button>
+  )
+}
+
+const DisplayCounter = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0);
+  // console.log(counter);
 
-  // setTimeout(
-  //   () => setCounter(counter + 1), 12000
-  // )
-  // console.log(useState(counter));
+  const decrementFunction = () => {
+    setCounter(counter - 1);
+  }
+
+  const incrementFunction = () => {
+    setCounter(counter + 1);
+  }
 
   return (
     <>
-      <p>{counter}</p>
+      <CounterButton onClick={incrementFunction} text='Increment' />
+      <DisplayCounter counter={counter} />
+      <CounterButton onClick={decrementFunction} text='Decrement' />
     </>
   )
 }
