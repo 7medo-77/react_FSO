628158acae5bb0f3e02ebb70c44ec2e2f84d03c1
hmedo <hmedo@7medo>
2024-11-07 15:56:20 +0200

    part 2a using array.map

---
+++ part2/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+// import './App.css'
+
+const App = (props) => {
+  const { notes } = props
+  console.log(notes);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map(note => {
+          return (
+            <li key={note.id} >{note.content}</li>
+          )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
