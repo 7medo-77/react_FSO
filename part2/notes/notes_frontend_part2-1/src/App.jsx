a52b6d552c872f745238b788217ce41f93b99f88
hmedo <hmedo@7medo>
2024-11-08 14:58:10 +0200

    part 2b notes eventhandler

---
+++ part2/notes/notes_frontend_part2-1/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true
+//   }
+// ]
+
+  const addNote = (event) => {
+    
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote} >
+        <fieldset>
+          <legend>Add note</legend>
+          <input value={newNote} onChange={handleNoteChange} />
+          <button type='submit'>save</button>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
+export default App 
